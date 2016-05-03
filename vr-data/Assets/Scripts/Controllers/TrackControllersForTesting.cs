ae482b8fec28843ad9e46116d403570ba0059297
Thomas Pritchard <hello@thomaspritchard.co.uk>
2016-05-03 14:22:45 +0100

    Tracking script now uses a sensible interval and uses timestamped files

--- vr-data/Assets/Scripts/Controllers/TrackControllersForTesting.cs
+++ vr-data/Assets/Scripts/Controllers/TrackControllersForTesting.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System;
 
 public class TrackControllersForTesting : MonoBehaviour {
 
@@ -51,21 +52,43 @@ public class TrackControllersForTesting : MonoBehaviour {
     public void EndTracking() {
         if (tracking) {
             tracking = false;
-            ExportDataToFile("test.csv");
+            ExportDataToFile();
         } else {
             Debug.LogError("Can't end a tracking session if one isn't started.");
         }
     }
 
-    public void ExportDataToFile(string filename) {
+    public void ExportDataToFile() {
+
+        string filename = "data-export-" + DateTime.Now.ToString("yyyy-MM-dd-H-mm-ss") + ".csv";
+
         if (File.Exists(filename)) {
             Debug.LogWarning(filename + " already exists. Overwriting it!");
             File.Delete(filename);
         }
         StreamWriter sw = File.CreateText(filename);
         sw.WriteLine("time,headMovement,leftControllerMovement,rightControllerMovement");
+
+        float exportInterval = 1f;
+        float nextExportTimeInSeconds = exportInterval;
+
+        float headMovSinceLastInterval = 0f;
+        float leftControllerMovSinceLastInterval = 0f;
+        float rightControllerMovSinceLastInterval = 0f;
+
         for (int i = 0; i < times.Count; i++) {
-            sw.WriteLine(times[i].ToString() + ", " + headMovements[i].ToString() + ", " + leftControllerMovements[i].ToString() + ", " + rightControllerMovements[i].ToString());
+            if (times[i] >= nextExportTimeInSeconds) {
+                sw.WriteLine(nextExportTimeInSeconds.ToString() + ", " + headMovSinceLastInterval.ToString() + ", " + leftControllerMovSinceLastInterval.ToString() + ", " + rightControllerMovSinceLastInterval.ToString());
+
+                headMovSinceLastInterval = 0f;
+                leftControllerMovSinceLastInterval = 0f;
+                rightControllerMovSinceLastInterval = 0f;
+                nextExportTimeInSeconds += exportInterval;
+            } else {
+                headMovSinceLastInterval += headMovements[i];
+                leftControllerMovSinceLastInterval += leftControllerMovements[i];
+                rightControllerMovSinceLastInterval += rightControllerMovements[i];
+            }
         }
         sw.Close();        
     }
